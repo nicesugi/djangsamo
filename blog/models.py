297caf578fb211fcded181ac9cf5441839c03f34
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-20 01:20:38 +0900

    Feat Blog 모델 생성 및 수정
    1. views.py
        ㄱ. Article - author null 추가 / 필드 default 설정
        ㄴ. Comment 생성
    2. urls.py
        ㄱ. 댓글 모델 함수와 url 연결
    3. 어드민 등록

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from user.models import User
 
 # Create your models here.
 class Category(models.Model):
@@ -10,12 +11,19 @@ class Category(models.Model):
     
     
 class Article(models.Model):
-    author = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name="사용자")
-    title = models.CharField("글제목", max_length=100)
-    category = models.ManyToManyField("Category", verbose_name="카테고리")
-    content = models.TextField("글내용")
+    author = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name="사용자",null=True, blank=True)
+    title = models.CharField("글제목", max_length=100, default='')
+    category = models.ManyToManyField('Category', verbose_name="카테고리", default='')
+    content = models.TextField("글내용", default='')
     
     def __str__(self):
         return f'{self.author}: {self.title}'
     
     
+class Comment(models.Model):
+    article = models.ForeignKey('Article', on_delete=models.CASCADE, verbose_name="댓글")
+    user = models.ForeignKey('user.User', on_delete=models.CASCADE, verbose_name="사용자")
+    content = models.TextField("댓글내용")
+    
+    def __str__(self):
+        return self.article
--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.ArticleView.as_view()),
+    path('comment/', views.CommentView.as_view()),
 ]
