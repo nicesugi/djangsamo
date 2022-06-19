3403fc56a15fcaaed31a161296956ad3fea25f38
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-20 01:13:04 +0900

    Feat User 함수 분리와 url 설정
    회원조회-등록-수정-탈퇴 > 127.0.0.1:8000/user/
    로그인-로그아웃 > 127.0.0.1:8000/user/login

--- user/urls.py
+++ user/urls.py
@@ -5,4 +5,5 @@ from user import views
 
 urlpatterns = [
     path('', views.UserView.as_view()),
+    path('login/', views.UserAPIView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,37 @@
 from rest_framework.views import APIView
+from rest_framework import permissions
 from rest_framework.response import Response
 from django.contrib.auth import login, logout, authenticate
 from rest_framework import status
-# from django.views.decorators.csrf import csrf_exempt
+
+from user.serializers import UserSerializer
 
 
-# @csrf_exempt
+# 127.0.0.1:8000/user/
 class UserView(APIView):
+    # permission_classes = [permissions.AllowAny]
+    # 사용자 조회
+    def get(self, request):
+        user=request.user
+        return Response(UserSerializer(user).data)
+    
+    # 회원가입
+    def post(self, request):
+        return Response({"msssage":"회원가입"})
+    
+    # 회원정보 수정
+    def put(self, request):
+        return Response({"msssage":"회원정보 수정"})
+    
+    # 회원탈퇴
+    def delete(self, request):
+        return Response({"message":"회원탈퇴"})
+    
+    
+    
+    
+# 127.0.0.1:8000/user/login/
+class UserAPIView(APIView):    
     # 로그인
     def post(self, request):
 
@@ -15,10 +40,10 @@ class UserView(APIView):
 
         user = authenticate(request, username=username, password=password)
         if not user:
-            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP.status)
+            return Response({"error": "존재하지 않는 계정이거나 패스워드가 일치하지 않습니다."}, status=status.HTTP_400_BAD_REQUEST)
 
         login(request, user)
-        print(f'{request.data} 의 user {user}')
+        print(f'{request.data} 로그인 성공') 
         return Response({"message": "로그인 성공!!"})
 
     # 로그아웃
