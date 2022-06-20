9a917a8528a0165ff45a8053aadb6910a4d77603
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-21 01:44:52 +0900

    Update permission class 커스텀
    admin은 모든 권한이 있고, 인증 된 사용자는 조회만 가능하도록 설정하기

--- Django/permissions.py
+++ Django/permissions.py
@@ -1,6 +1,8 @@
 from rest_framework.permissions import BasePermission
 from datetime import datetime, timedelta
 from django.utils import timezone
+from rest_framework.exceptions import APIException
+from rest_framework import status
 
 
 class RegistedMoreThanThreeDaysUser(BasePermission):
@@ -21,3 +23,37 @@ class RegistedMoreThanThreeDaysUser(BasePermission):
         print(f'3일 전 = {three_days_ago}')
 
         return bool(join_date < three_days_ago)
+
+
+class GenericAPIException(APIException):
+    def __init__(self, status_code, detail=None, code=None):
+        self.status_code=status_code
+        super().__init__(detail=detail, code=code)
+
+
+class IsAdminOrIsAuthenticatedReadOnly(BasePermission):
+    """
+    admin 사용자는 모두 가능, 로그인 사용자는 조회만 가능
+    """
+    SAFE_METHODS = ('GET', )
+    message = '접근 권한이 없습니다.'
+
+    def has_permission(self, request, view):
+        user = request.user
+
+        if not user.is_authenticated:
+            response ={
+                    "detail": "서비스를 이용하기 위해 로그인 해주세요.",
+                }
+            raise GenericAPIException(status_code=status.HTTP_401_UNAUTHORIZED, detail=response)
+
+        # 로그인 사용자가 get 요청 시 True
+        if user.is_authenticated and request.method in self.SAFE_METHODS:
+            return True
+        
+        # admin 사용자이거나 가입일이 7일 이상 된 사용자의 경우 True
+        if user.is_authenticated and user.is_admin or \
+            user.join_date < (datetime.now().date() - timedelta(days=7)):
+            return True
+            
+        return False
