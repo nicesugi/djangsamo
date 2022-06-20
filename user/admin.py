23065c0135c762f407718031e96edaada1c491d5
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-21 01:54:07 +0900

    Feat UserProfileInline, UserAdmin 생성
    userprofile 테이블을 user admin 페이지에서 같이 보고 설정

--- user/admin.py
+++ user/admin.py
@@ -1,7 +1,37 @@
 from django.contrib import admin
-from .models import User, UserProfile
+from user.models import User as UserModel
+from user.models import UserProfile as UserProfileModel
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
 
-admin.site.register(User)
-admin.site.register(UserProfile)
+class UserProfileInline(admin.StackedInline):
+    model = UserProfileModel
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('id', 'username', 'fullname', 'email', )
+    list_display_links = ('username', )
+    list_filter = ('username', )
+    search_fields = ('username', 'email', )
+    # readonly_fields = ('join_date', )
+    
+    fieldsets = (
+        ("info", {"fields": ('username', 'password', 'email', 'fullname', 'join_date', )}),
+        ("Permissions", {'fields': ('is_admin', 'is_active', )}),
+        )
+
+    filter_horizontal = []
+    
+    def get_readonly_fields(self, request, obj=None):
+        print(obj)
+        if obj:
+            return ('username', 'join_date', )
+        else:
+            return ('join_date', )
+        
+    inlines = (
+            UserProfileInline,
+        )
+      
+# admin.site.register(User)
+admin.site.register(UserModel, UserAdmin)
+admin.site.register(UserProfileModel)
