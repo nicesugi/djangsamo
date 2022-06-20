b3769cfdb390679a5537e5b3cb26c645bcd5a948
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-21 01:42:30 +0900

    실행되는 SQL 쿼리 보기

--- Django/settings.py
+++ Django/settings.py
@@ -57,6 +57,23 @@ REST_FRAMEWORK = {
     ]
 }
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
