4b21348e7592b135bb23aa98635796017022e597
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-21 19:17:32 +0900

    Refactor 시리얼라이저 분리(user->blog)
    1. blog에 해당되는 시리얼라이저 함수 이동시킴
    2. 카테고리 반영
    3. Meta fields 내용 수정
    4. ArticleSerializer 에 extra_kwargs 생성하여 필드에 다양한 옵션 지정

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,45 +1,47 @@
-# from dataclasses import field
-# from xml.etree.ElementTree import Comment
-# from rest_framework import serializers
-# # from user.models import User as UserModel
-# # from user.models import UserProfile as UserProfileModel
-# from blog.models import Article as ArticleModel
-# from blog.models import Comment as CommentModel
-
-# class ArticleSerializer(serializers.ModelSerializer):
-#     articles_user = serializers.SerializerMethodField()
-#     def get_articles_user(self, obj):
-#         return obj.author.fullname
-
-#     class Meta:
-#         model = ArticleModel
-#         fields = ["title", "category", "content", "articles_user"]
+from rest_framework import serializers
+from blog.models import Comment as CommentModel
+from blog.models import Article as ArticleModel
 
 
-# class CommentSerializer(serializers.ModelSerializer):
-#     comments_user = serializers.SerializerMethodField()
-#     def get_comments_user(self, obj):
-#         return obj.user.username
+
+class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField()
+    def get_user(self, obj):
+        return obj.user.username
     
-#     class Meta:
-#         model = CommentModel
-#         fields = ["article", "content", "comments_user"]
-
-
-# # class UserProfileSerializer(serializers.ModelSerializer):
-# #     class Meta:
-# #         model = UserProfileModel
-# #         fields = ["introduction", "birthday", "age"]
+    class Meta:
+        model = CommentModel
+        fields = ["user", "content"]
         
         
-# # class UserSerializer(serializers.ModelSerializer):
-# #     article_set = ArticleSerializer(many=True)
-# #     comment_set = CommentSerializer(many=True)
-# #     userprofile = UserProfileSerializer() 
+class ArticleSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    comments = CommentSerializer(many=True, source="comment_set", read_only=True)
     
-# #     class Meta:
-# #         model = UserModel
-# #         fields = [
-# #             "id", "last_login", "username", "email", 
-# #             "userprofile", "article_set", "comment_set"
-# #         ]
+    
+    def get_category(self, obj):
+        return [category.name for category in obj.category.all()]
+
+    class Meta:
+        model = ArticleModel
+        fields = ["author", "category", "title", "content", "comments", 
+                  "exposure_start_date", "exposure_end_date"
+                  ]
+        
+        # 각 필드에 해당하는 다양한 옵션 지정
+        extra_kwargs = {
+            # write_only : 해당 필드를 쓰기 전용으로 만들어 준다.
+            # 쓰기 전용으로 설정 된 필드는 직렬화 된 데이터에서 보여지지 않는다.
+            'user': {'write_only': True}, # default : False
+            'email': {
+                # error_messages : 에러 메세지를 자유롭게 설정 할 수 있다.
+                'error_messages': {
+                    # required : 값이 입력되지 않았을 때 보여지는 메세지
+                    'required': '이메일을 입력해주세요.',
+                    # invalid : 값의 포맷이 맞지 않을 때 보여지는 메세지
+                    'invalid': '알맞은 형식의 이메일을 입력해주세요.'
+                    },
+                    # required : validator에서 해당 값의 필요 여부를 판단한다.
+                    'required': False # default : True
+                    },
+            }
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,7 @@ from rest_framework.views import APIView
 from rest_framework import permissions, status
 from rest_framework.response import Response
 from blog.models import Article as ArticleModel
-from .models import Comment
-from user.models import User
+
 from user.serializers import ArticleSerializer, UserSerializer
 from Django.permissions import RegistedMoreThanThreeDaysUser
 from Django.permissions import IsAdminOrIsAuthenticatedReadOnly
@@ -15,10 +14,6 @@ from datetime import datetime
 
 class ArticleView(APIView):
     # 게시글 조회
-    # def get(self, request):
-    #     user=request.user
-
-    #     return Response(UserSerializer(user).data) 
     def get(self, request):
         permission_classes = [IsAdminOrIsAuthenticatedReadOnly]
         today = datetime.now()
@@ -29,51 +24,39 @@ class ArticleView(APIView):
         
         return Response(ArticleSerializer(articles, many=True).data, status=status.HTTP_200_OK)
     
-    # 게시글 작성 (3일 이상 지난 사용자만 게시글 작성 가능)
+    
     def post(self, request):
-        permission_classes = [RegistedMoreThanThreeDaysUser]
         user = request.user
-        title = request.data.get('title', '')
-        content = request.data.get('content', '')
-        categorys = request.data.pop('category', [])
+        request.data['user'] = user.id
+        article_serializer = ArticleSerializer(data=request.data)
         
-        if len(title) <= 5:
-            return Response({"error": "타이틀은 5자 이상 작성해주세요"}, status=status.HTTP_400_BAD_REQUEST)
-        if len(content) <= 20:
-            return Response({"error": "내용은 20자 이상 작성해주세요"}, status=status.HTTP_400_BAD_REQUEST)
-        if not categorys:
-            return Response({"error": "카테고리가 지정되지 않았습니다"}, status=status.HTTP_400_BAD_REQUEST)
-        article = ArticleModel(
-            user=user,
-            **request.data
-        )
-        article.save()
-        article.category.add(*categorys)
-        return Response({"message": "성공"}, status=status.HTTP_200_OK)
+        if article_serializer.is_valid():
+            article_serializer.save()
+            return Response(article_serializer.data, status=status.HTTP_200_OK)
         
-        # author = request.user
-        # title = request.data.get('title', '')
-        # category = request.data.get('category', '')
-        # content = request.data.get('content', '')
+        return Response(article_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    # # 게시글 작성 (3일 이상 지난 사용자만 게시글 작성 가능)
+    # def post(self, request):
+    #     permission_classes = [RegistedMoreThanThreeDaysUser]
+    #     user = request.user
+    #     title = request.data.get('title', '')
+    #     content = request.data.get('content', '')
+    #     categorys = request.data.pop('category', [])
         
-        # article, created = ArticleModel.objects.get_or_create(
-        #     title = title,
-        #     content = content,
-        #     )     
-
-        # if created:
-        #     article.author = author
-        #     article.category.add(category)
-        #     article.title = title
-        #     article.content = content
-        #     article.save()
-        #     return Response({"message": "게시글 작성 성공!!"}, status=status.HTTP_200_OK)
+    #     if len(title) <= 5:
+    #         return Response({"error": "타이틀은 5자 이상 작성해주세요"}, status=status.HTTP_400_BAD_REQUEST)
+    #     if len(content) <= 20:
+    #         return Response({"error": "내용은 20자 이상 작성해주세요"}, status=status.HTTP_400_BAD_REQUEST)
+    #     if not categorys:
+    #         return Response({"error": "카테고리가 지정되지 않았습니다"}, status=status.HTTP_400_BAD_REQUEST)
+    #     article = ArticleModel(
+    #         user=user,
+    #         **request.data
+    #     )
+    #     article.save()
+    #     article.category.add(*categorys)
+    #     return Response({"message": "성공"}, status=status.HTTP_200_OK)
         
-        # elif len(title) < 5 or len(content) < 20 or category =='':
-        #     return Response({"message": "조건이 충족되지 않았습니다!!"}, status=status.HTTP_411_LENGTH_REQUIRED)
-        # else:
-        #     return Response({"message":"이미 작성된 글입니다"}, status=status.HTTP_400_BAD_REQUEST)
-
 
 
 class CommentView(APIView):
