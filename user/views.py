0194b5f464c095c5b12856427cd661df6a774d1a
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-21 19:13:39 +0900

    Feat UserView 수정
    0. IsAdminOrIsAuthenticatedReadOnly 임포트 추가
    1. 사용자조회 수정
        ㄱ. permission 사용
        ㄴ. context에 요청담아서 시리얼라이저로 데이터를 리턴
    2. 회원가입 생성
    3. 회원정보 수정 생성

--- user/views.py
+++ user/views.py
@@ -5,23 +5,43 @@ from django.contrib.auth import login, logout, authenticate
 from rest_framework import status
 
 from user.serializers import UserSerializer
+from Django.permissions import IsAdminOrIsAuthenticatedReadOnly
+from user.models import User as UserModel
 
 
 # 127.0.0.1:8000/user/
 class UserView(APIView):
-    # permission_classes = [permissions.AllowAny]
+    
     # 사용자 조회
     def get(self, request):
-        user=request.user
-        return Response(UserSerializer(user).data)
+        permission_classes = [IsAdminOrIsAuthenticatedReadOnly]
+        user_serializer = UserSerializer(request.user, context={"request": request}).data
+        return Response(user_serializer, status=status.HTTP_200_OK)
     
     # 회원가입
     def post(self, request):
-        return Response({"msssage":"회원가입"})
+        user_serializer = UserSerializer(data=request.data, context={"request": request})
+        
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # 회원정보 수정
-    def put(self, request):
-        return Response({"msssage":"회원정보 수정"})
+    def put(self, request, obj_id):
+        user = UserModel.objects.get(id=obj_id)
+        user_serializer = UserSerializer(user, data=request.data, partial=True, context={"request": request})
+        ## 비밀번호 수정
+        # if password == check_password:
+        #     pass
+        # else:
+        #     error : 입력한 패스워드가 일치하지 않습니다.
+            
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     # 회원탈퇴
     def delete(self, request):
@@ -32,9 +52,9 @@ class UserView(APIView):
     
 # 127.0.0.1:8000/user/login/
 class UserAPIView(APIView):    
+    
     # 로그인
     def post(self, request):
-
         username = request.data.get('username', '')
         password = request.data.get('password', '')
 
