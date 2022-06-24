ec7dbb0155aace282cb2b902475b5741a28944a8
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-25 01:21:15 +0900

    Update permissions.py

--- Django/permissions.py
+++ Django/permissions.py
@@ -1,3 +1,4 @@
+import re
 from rest_framework.permissions import BasePermission
 from datetime import datetime, timedelta
 from django.utils import timezone
@@ -10,19 +11,39 @@ class RegistedMoreThanThreeDaysUser(BasePermission):
     가입일 기준 3일 이상 지난 사용자만 접근 가능
     """
     
+    SAFT_METHODS = ('GET', )
+    message = '가입 후 3일 이상 지난 사용자만 사용하실 수 있습니다'
+
     def has_permission(self, request, view):
         user = request.user
-        if not user or not user.is_authenticated:
-            return False
-        # Date field : 2020-10-2join_date.strftime("%Y-%m-%d"))
-        # DateTime field : 2020-10-2 10:20:32
-        join_date = user.join_date
-        three_days_ago = timezone.now() - timedelta(days=3)
-        print(f'가입한 날짜 = {join_date}')
-        print(f'지금 날짜 = {timezone.now()}')
-        print(f'3일 전 = {three_days_ago}')
+        
+        if not user.is_authenticated:
+            response = {
+                "detail": "서비스를 이용하기 위해 로그인 해주세요.",
+            }
+            raise GenericAPIException(status=status.HTTP_401_UNAUTHORIZED, detail=response)
+        
+        # 로그인 사용자가 get 요청시 True
+        if user.is_authenticated and request.method in self.SAFT_METHODS:
+            return True        
+    
+        return bool(user.is_authenticated and
+                    request.user.join_date < (timezone.now() - timedelta(days=3)))
+        
+        
+        # if not user or not user.is_authenticated:
+        #     return False
+        # # Date field : 2020-10-2join_date.strftime("%Y-%m-%d"))
+        # # DateTime field : 2020-10-2 10:20:32
+        # join_date = user.join_date
+        # three_days_ago = timezone.now() - timedelta(days=3)
+        # print(f'가입한 날짜 = {join_date}')
+        # print(f'지금 날짜 = {timezone.now()}')
+        # print(f'3일 전 = {three_days_ago}')
 
-        return bool(join_date < three_days_ago)
+        # return bool(join_date < three_days_ago)
+        
+        
 
 
 class GenericAPIException(APIException):
